9775d5dd682843f962d30cfd0857ea698d4302f0
Daniel Zarinski <daniel.zarinski@akinox.com>
2026-02-27 14:05:41 -0700

    fix: update renderer application name in constants
    - Changed the rendererAppName constant from 'Time Tracker' to 'renderer' for better clarity and alignment with application functionality.

--- apps/desktop/src/app/constants.ts
+++ apps/desktop/src/app/constants.ts
@@ -10,6 +10,6 @@ export const WINDOW = {
 export const MENU_MAX_LENGTH = 50;
 
 export const rendererAppPort = 4200;
-export const rendererAppName = 'Time Tracker'; // options.name.split('-')[0] + '-web'
+export const rendererAppName = 'renderer';
 export const electronAppName = 'Time Tracker';
 export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
